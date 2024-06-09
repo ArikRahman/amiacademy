c2a5f0f66a5218eddac6f887e0f05262855b5e23
Arik Rahman <arik@tamu.edu>
2024-06-08 19:41:13 -0500

    Added suggestions

--- lib/features/math/presentation/screens/math_screen.dart
+++ lib/features/math/presentation/screens/math_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:amiacademy/features/math/presentation/widgets/math_question_widget.dart';
+// import 'package:amiacademy/features/math/presentation/widgets/math_question_widget.dart';
 import 'package:amiacademy/features/math/presentation/widgets/derivatives_practice.dart';
 
 class MathScreen extends StatelessWidget {
+  const MathScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Ami Academy'),
+        title: const Text('Ami Academy'),
       ),
-      body: Center(
+      body: const Center(
         // child: MathQuestionWidget(),
         child: DerivativeQuestionWidget(),
       ),
